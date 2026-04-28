8f3d0f1ec5c121b3bb38a94a32082354d96642f9
Biznet Subscription <biznetsubscription@Biznets-Mac-mini-Prod-4.local>
2026-04-28 18:32:01 +0800

    added tts url based on ip's

--- src/agent.py
+++ src/agent.py
@@ -77,7 +77,7 @@ async def my_agent(ctx: agents.JobContext):
         ),
         tts=openai.TTS(
             model='tts-1',
-            # base_url='http://127.0.0.1:8000/v1',
+            # base_url='http://192.168.134.138:8000/v1',
             base_url='http://127.0.0.1:8000/v1',
             api_key='empty',
             response_format='wav'

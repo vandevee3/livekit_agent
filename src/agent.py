fbe53fba6a9606e349eb75779cb5db61430d5b84
Biznet Subscription <biznetsubscription@Biznets-Mac-mini-Prod-4.local>
2026-04-29 16:56:05 +0800

    create better run.txt

--- src/agent.py
+++ src/agent.py
@@ -114,6 +114,7 @@ async def my_agent(ctx: agents.JobContext):
             base_url='http://127.0.0.1:8000/v1',
             api_key='empty',
             response_format='wav',
+            # voice='pace_kobo_clean.wav'
             # client=tts_client
             # voice='alloy'
         ),
